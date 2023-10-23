77a9d52b91a846fc9f4c48ca6f6d95485771bbcd
Quinn Herden <55929299+QuinnHerden@users.noreply.github.com>
2023-10-23 13:12:12 -0400

    flesh out deployment definition variable access

--- main.tf
+++ main.tf
@@ -1,7 +1,22 @@
 module "vault" {
-  source         = "terraform-google-modules/vault/google"
-  project_id     = var.project_id
-  region         = var.region
-  kms_keyring    = var.kms_keyring
-  kms_crypto_key = var.kms_crypto_key
+  source = "terraform-google-modules/vault/google"
+
+  allow_ssh                    = var.allow_ssh
+  domain                       = var.domain 
+  host_project_id              = var.host_project_id
+  load_balancing_scheme        = var.load_balancing_scheme
+  network                      = var.network
+  project_id                   = var.project_id
+  region                       = var.region
+  service_label                = var.service_label
+  ssh_allowed_cidrs            = var.ssh_allowed_cidrs
+  storage_bucket_force_destroy = var.storage_bucket_force_destroy
+  tls_ca_subject               = var.tls_ca_subject
+  vault_log_level              = var.vault_log_level
+  vault_machine_type           = var.vault_machine_type
+  vault_max_num_servers        = var.vault_max_num_servers
+  vault_min_num_servers        = var.vault_min_num_servers
+  vault_ui_enabled             = var.vault_ui_enabled
+  vault_update_policy_type     = var.vault_update_policy_type
+  vault_version                = var.vault_version
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,130 @@
+
+variable "allow_ssh" {
+  type        = bool
+  default     = false
+  description = "Allow external access to ssh port 22 on the Vault VMs. It is a best practice to set this to false, however it is true by default for the sake of backwards compatibility"
+}
+
+variable "domain" {
+  type        = string
+  default     = ""
+  description = "The domain name that will be set in the api_addr. Load Balancer IP used by default"
+}
+
+variable "host_project_id" {
+  type        = string
+  default     = ""
+  description = "The project id of the shared VPC host project, when deploying into a shared VPC"
+}
+
+variable "load_balancing_scheme" {
+  type        = string
+  default     = "EXTERNAL"
+  description = "Options are INTERNAL or EXTERNAL. If EXTERNAL, the forwarding rule will be of type EXTERNAL and a public IP will be created. If INTERNAL the type will be INTERNAL and a random RFC 1918 private IP will be assigned"
+}
+
+variable "network" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
 variable "project_id" {
   type        = string
-  description = "GCP project id"
+  description = "ID of the project in which to create resources and add IAM bindings"
 }
 
 variable "region" {
+  type        = string
   default     = "us-east4"
   description = "GCP region in which to create resources"
+}
+
+variable "service_label" {
+  type        = string
+  default     = ""
+  description = "The service label to set on the internal load balancer. If not empty, this enables internal DNS for internal load balancers. By default, the service label is disabled. This has no effect on external load balancers"
+}
+
+variable "ssh_allowed_cidrs" {
+  type = list(string)
+  default = [
+    "0.0.0.0/0"
+  ]
+  description = "List of CIDR blocks to allow access to SSH into nodes"
+}
+
+variable "storage_bucket_force_destroy" {
+  type        = string
+  default     = "false"
+  description = "Set to true to force deletion of backend bucket on terraform destroy"
+}
+
+variable "tls_ca_subject" {
+  type = object({
+    common_name         = string,
+    organization        = string,
+    organizational_unit = string,
+    street_address      = list(string),
+    locality            = string,
+    province            = string,
+    country             = string,
+    postal_code         = string,
+  })
+  default = {
+    "common_name" : "Example Inc. Root",
+    "country" : "US",
+    "locality" : "The Intranet",
+    "organization" : "Example, Inc",
+    "organizational_unit" : "Department of Certificate Authority",
+    "postal_code" : "95559-1227",
+    "province" : "CA",
+    "street_address" : [
+      "123 Example Street"
+    ]
+  }
+  description = "The subject block for the root CA certificate"
+}
+
+variable "vault_log_level" {
+  type        = string
+  default     = "warn"
+  description = "Log level to run Vault in. See the Vault documentation for valid values"
+}
+
+variable "vault_machine_type" {
+  type        = string
+  default     = "e2-standard-2"
+  description = "Machine type to use for Vault instances"
+}
+
+variable "vault_max_num_servers" {
+  type        = string
+  default     = "7"
+  description = "Maximum number of Vault server nodes to run at one time. The group will not autoscale beyond this number"
+}
+
+variable "vault_min_num_servers" {
+  type        = string
+  default     = "1"
+  description = "Minimum number of Vault server nodes in the autoscaling group. The group will not have less than this number of nodes"
+}
+
+variable "vault_ui_enabled" {
+  type        = string
+  default     = "true"
+  description = "Controls whether the Vault UI is enabled and accessible"
+}
+
+
+variable "vault_update_policy_type" {
+  type        = string
+  default     = "OPPORTUNISTIC"
+  description = "Options are OPPORTUNISTIC or PROACTIVE. If PROACTIVE, the instance group manager proactively executes actions in order to bring instances to their target versions"
+}
+
+variable "vault_version" {
+  type        = string
+  default     = "1.6.0"
+  description = "Version of vault to install. This version must be 1.0+ and must be published on the HashiCorp releases service"
 }
